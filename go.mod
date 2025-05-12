f2b540105b8df5aea12cd3b7f285e3da3d470dfd
yutian <yutian@staruniongame.com>
2025-05-12 20:47:17 +0800

    feat: milvusc

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ydx1011/gopher-milvusc
+
+go 1.24.3
+
+require (
+	github.com/ydx1011/gopher-core v0.0.4
+	github.com/milvus-io/milvus/client/v2 v2.5.1
+)
---
+++ milvusc/processor.go
@@ -0,0 +1,63 @@
+package milvusc
+
+import (
+	"context"
+	"github.com/milvus-io/milvus/client/v2/milvusclient"
+	"github.com/xfali/xlog"
+	"github.com/ydx1011/gopher-core/bean"
+	"github.com/ydx1011/yfig"
+)
+
+const (
+	BuildinValueMilvuscSources = "gopher.milvusc"
+)
+
+type Sources struct {
+	Address  string
+	Username string
+	Password string
+}
+type Processor struct {
+	logger xlog.Logger
+}
+
+func (p *Processor) Init(conf yfig.Properties, container bean.Container) error {
+	dss := map[string]*Sources{}
+	err := conf.GetValue(BuildinValueMilvuscSources, &dss)
+	if len(dss) == 0 {
+		p.logger.Errorln("No Database")
+		return nil
+	}
+	for k, v := range dss {
+		client, err := milvusclient.New(context.Background(), &milvusclient.ClientConfig{
+			Address:  v.Address,
+			Username: v.Username,
+			Password: v.Password,
+		})
+		if err != nil {
+			p.logger.Errorln(err)
+			return nil
+		}
+		//添加到注入容器
+		container.RegisterByName(k, client)
+	}
+
+	return err
+}
+
+func (p *Processor) Process() error {
+	return nil
+}
+
+func (p *Processor) Classify(o interface{}) (bool, error) {
+	//switch v := o.(type) {
+	//case redis.Client:
+	//	err := p.parseBean(v)
+	//	return true, err
+	//}
+	return false, nil
+}
+
+func (p *Processor) BeanDestroy() error {
+	return nil
+}
